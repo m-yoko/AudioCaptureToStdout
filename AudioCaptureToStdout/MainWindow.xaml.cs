44fc9a59e5164e2638685a858f725dc638ba1b70
m-yoko <39024974+m-yoko@users.noreply.github.com>
2019-11-29 17:40:57 +0900

    add main

---
+++ AudioCaptureToStdout/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace AudioCaptureToStdout
+{
+    public enum RadioButtonMode
+    {
+        NAudio,
+        AccordNet
+    }
+
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartStopButtonClick(object sender, RoutedEventArgs e)
+        {
+            foreach (System.Windows.Controls.RadioButton rButton in radioButtonStackPannel.Children)
+            {
+                Debug.WriteLine(rButton.ToString());
+            }
+        }
+    }
+}
